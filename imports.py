0e2447d581f3b07e5e19a676af05d1936da24371
Владислав Бусов <busovrm4@gmail.com>
2025-02-11 17:24:02 +0300

    Build start structure

---
+++ imports.py
@@ -0,0 +1,15 @@
+from aiogram.filters import Command, StateFilter
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from aiogram import F, Router
+
+from time import strptime
+
+from main import dp, bot
+from config import MSGS, BUTTONS
+from keyboards import *
+from states import *
+from database import *
+
+main_router = Router()
+dp.include_router(main_router)
